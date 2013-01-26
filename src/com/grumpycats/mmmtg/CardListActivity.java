f692ba7ad552a0e479389b80e37dcf3fa9838639
Ilariia Belova <ilariyabelova@gmail.com>
2013-01-26 04:17:15 +0200

    Card structure with json parsing

--- src/com/grumpycats/mmmtg/CardListActivity.java
+++ src/com/grumpycats/mmmtg/CardListActivity.java
@@ -2,14 +2,34 @@ package com.grumpycats.mmmtg;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.sql.Timestamp;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CardListActivity extends Activity {
+
+    private static final String TAG = "com.grumpycats.mmmtg.CardListActivity";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.stock_list);
+        Map<Timestamp, Double> d = new HashMap<Timestamp, Double>();
+        d.put(new Timestamp(1), 21.2);
+        System.out.println(d);
+        Card c = Card.create("hello", "world", d, 3, 4);
+        System.out.println(c);
+        try{
+            c = Card.valueOf(new JSONObject("{id:1,name:'Force of Will',block:Revised,prices:{}}"));
+            System.out.println(c);
+        } catch (JSONException e){
+            Log.e(TAG, "JSON exception", e);
+        }
     }
 
     @Override
